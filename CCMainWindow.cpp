771d74f09b2daf10f22d350ee083b3c8ffc45715
iamxiewei <1121419718@qq.com>
2024-04-09 23:33:48 +0800

    提交QtQQ-06

--- CCMainWindow.cpp
+++ CCMainWindow.cpp
@@ -1,7 +1,179 @@
 #include "CCMainWindow.h"
+#include "SkinWindow.h"
+#include "SysTray.h"
+
+#include <QProxyStyle>
+#include <QPainter>
+#include <QTimer>
+
+
+class CustomProxyStyle :public QProxyStyle
+{
+public:
+	virtual void drawPrimitive(PrimitiveElement element, const QStyleOption* option,
+		QPainter* painter, const QWidget* widget = nullptr) const
+	{
+		if (element == PE_FrameFocusRect)
+		{
+			return;
+		}
+		else
+		{
+			QProxyStyle::drawPrimitive(element, option, painter, widget);
+		}
+	}
+};
 
 CCMainWindow::CCMainWindow(QWidget *parent)
-	: QWidget(parent)
+	: BasicWindow(parent)
 {
 	ui.setupUi(this);
+	setWindowFlags(windowFlags() | Qt::Tool);
+	loadStyleSheet("CCMainWindow");
+	initControl();
+	initTimer();
+}
+
+CCMainWindow::~CCMainWindow()
+{
+}
+
+void CCMainWindow::initTimer()
+{
+	QTimer *timer = new QTimer(this);
+	timer->setInterval(500);
+
+	connect(timer, &QTimer::timeout,[this]{
+		static int level = 0;
+		if (level == 99)
+		{
+			level = 0;
+		}
+		level++;
+		setLevelPixmap(level);
+	});
+
+	timer->start();
+}
+
+void CCMainWindow::initControl()
+{
+	//树获取焦点时不绘制边框
+	ui.treeWidget->setStyle(new CustomProxyStyle);
+	setLevelPixmap(0);
+	setHeadPixmap(":/Resources/MainWindow/girl.png");
+	setStatusMenuIcon(":/Resources/MainWindow/StatusSucceeded.png");
+
+	QHBoxLayout *appupLayout = new QHBoxLayout;
+	appupLayout->setContentsMargins(0, 0, 0, 0);
+	appupLayout->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_7.png", "app_7"));
+	appupLayout->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_2.png", "app_2"));
+	appupLayout->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_3.png", "app_3"));
+	appupLayout->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_4.png", "app_4"));
+	appupLayout->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_5.png", "app_5"));
+	appupLayout->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_6.png", "app_6"));
+	appupLayout->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/skin.png", "app_skin"));
+	appupLayout->addStretch();
+	appupLayout->setSpacing(2);
+	ui.appWidget->setLayout(appupLayout);
+
+	ui.bottomLayout_up->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_10.png", "app_10"));
+	ui.bottomLayout_up->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_8.png", "app_8"));
+	ui.bottomLayout_up->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_11.png", "app_11"));
+	ui.bottomLayout_up->addWidget(addOtherAppExtension(":/Resources/MainWindow/app/app_9.png", "app_9"));
+	ui.bottomLayout_up->addStretch();
+
+	connect(ui.sysmin, SIGNAL(clicked(bool)), this, SLOT(onShowHide(bool)));
+	connect(ui.sysclose, SIGNAL(clicked(bool)), this, SLOT(onShowClose(bool)));
+
+	SysTray* systray = new SysTray(this);
+}
+
+void CCMainWindow::setUserName(const QString & username)
+{
+	ui.nameLabel->adjustSize();
+
+	//文本过长则进行省略...
+	//fontMetrics()返回QFontMetrics类对象
+ 	QString name =  ui.nameLabel->fontMetrics().elidedText(username, Qt::ElideRight, ui.nameLabel->width());
+
+	ui.nameLabel->setText(name);
+}
+
+void CCMainWindow::setLevelPixmap(int level)
+{
+	QPixmap levelPixmap(ui.levelBtn->size());
+	levelPixmap.fill(Qt::transparent);
+
+	QPainter painter(&levelPixmap);
+	painter.drawPixmap(0, 4, QPixmap(":/Resources/MainWindow/lv.png"));
+
+	int unitNum = level % 10;//个位数
+	int tenNum = level / 10; //十位数
+
+	//十位,截取图片中的部分进行绘制
+	//drawPixmap(绘制点x，绘制点y,图片，图片左上角x,图片左上角y,拷贝的宽度，拷贝的高度)
+	painter.drawPixmap(10, 4, QPixmap(":/Resources/MainWindow/levelvalue.png"), tenNum * 6, 0, 6, 7);
+
+	//个位
+	painter.drawPixmap(16, 4, QPixmap(":/Resources/MainWindow/levelvalue.png"), unitNum * 6, 0, 6, 7);
+
+	ui.levelBtn->setIcon(levelPixmap);
+	ui.levelBtn->setIconSize(ui.levelBtn->size());
+}
+
+void CCMainWindow::setHeadPixmap(const QString & headPath)
+{
+	QPixmap pix;
+	pix.load(":/Resources/MainWindow/head_mask.png");
+	ui.headLabel->setPixmap(getRoundImage(QPixmap(headPath), pix, ui.headLabel->size()));
+}
+
+void CCMainWindow::setStatusMenuIcon(const QString & statusPath)
+{
+	QPixmap statusBtnPixmap(ui.stausBtn->size());
+	statusBtnPixmap.fill(Qt::transparent);
+
+	QPainter painter(&statusBtnPixmap);
+	painter.drawPixmap(4, 4, QPixmap(statusPath));
+
+	ui.stausBtn->setIcon(statusBtnPixmap);
+	ui.stausBtn->setIconSize(ui.stausBtn->size());
+}
+
+QWidget * CCMainWindow::addOtherAppExtension(const QString & appPath, const QString & appName)
+{
+	QPushButton* btn = new QPushButton(this);
+	btn->setFixedSize(20, 20);
+
+	QPixmap pixmap(btn->size());
+	pixmap.fill(Qt::transparent);
+
+	QPainter painter(&pixmap);
+	QPixmap appPixmap(appPath);
+	painter.drawPixmap( (btn->width() - appPixmap.width()) / 2,
+		(btn->height() - appPixmap.height()) / 2, appPixmap);
+	btn->setIcon(pixmap);
+	btn->setIconSize(btn->size());
+	btn->setObjectName(appName);
+	btn->setProperty("hasborder", true);
+
+	connect(btn, &QPushButton::clicked, this, &CCMainWindow::onAppIconClicked);
+	return btn;
+}
+
+void CCMainWindow::resizeEvent(QResizeEvent * event)
+{
+	setUserName(QString::fromLocal8Bit("奇牛科技-越分享越拥有"));
+	BasicWindow::resizeEvent(event);
+}
+
+void CCMainWindow::onAppIconClicked()
+{
+	//判断信号发送者的对象名是否是app_skin
+	if (sender()->objectName() == "app_skin")
+	{
+		SkinWindow* skinWindow = new SkinWindow;
+		skinWindow->show();
+	}
 }
